220cac85910f4e89eef5c4fdd2d92ff8313b90a2
tony <serratedhumor@yahoo.com>
2019-12-05 12:56:14 -0600

    test implemented to check for rendering of all elements

--- baseball-scoreboard/src/App.test.js
+++ baseball-scoreboard/src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders baseball scoreboard', () => {
+  const { getByText, getAllByText } = render(<App />);
+  const strikeElement = getAllByText(/strike/i);
+  const ballElement = getAllByText(/ball/i);
+  const foulElement = getByText(/foul/i);
+  const hitElement = getByText(/hit/i);
+  strikeElement.forEach((e) => expect(e).toBeInTheDocument())
+  ballElement.forEach((e) => expect(e).toBeInTheDocument())
+  expect(foulElement).toBeInTheDocument();
+  expect(hitElement).toBeInTheDocument();
 });
