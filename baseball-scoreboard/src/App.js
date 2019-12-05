411a9d635339d880221c7af33ceb06bdf7120349
tony <serratedhumor@yahoo.com>
2019-12-05 12:01:36 -0600

    initiated project

---
+++ baseball-scoreboard/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [allState, editState] = React.useState({
+    strikes: 0,
+    balls: 0
+  })
+
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
